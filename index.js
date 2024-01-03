72e3844fbf0629f906d33a4d69fc49411993cd97
divyeshdabhi <divyeshd623@gmail.com>
2024-01-03 10:55:46 +0530

    add cors

--- index.js
+++ index.js
@@ -9,7 +9,13 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-app.use(cors());
+app.use(cors(
+  {
+    origin:["https://node-mongodb-api-b4pb.onrender.com"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
 
 app.get("/", (req, res) => {
   return res
